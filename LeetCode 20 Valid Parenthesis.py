47f3ac2f009a0b32940baea506cdcebd6bc4e351
Alli Sodeeq <sodeeqalli@gmail.com>
2024-08-21 18:38:46 +0100

    Solved question 70

--- LeetCode 20 Valid Parenthesis.py
+++ LeetCode 20 Valid Parenthesis.py
@@ -1,3 +1,43 @@
+# 20. Valid Parentheses
+# Solved
+# Easy
+# Topics
+# Companies
+# Hint
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+# An input string is valid if:
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+# Every close bracket has a corresponding open bracket of the same type.
+ 
+# Example 1:
+# Input: s = "()"
+# Output: true
+# Example 2:
+
+# Input: s = "()[]{}"
+# Output: true
+# Example 3:
+
+# Input: s = "(]"
+# Output: false
+ 
+
+# Constraints:
+# 1 <= s.length <= 104
+# s consists of parentheses only '()[]{}'.
+
+#Thought process
+        #If stack is empty and we encounter a closing bracket, just return false, if not in map, we append
+        #if its not empty, we append if its not in map or its in map but doesnt match top of stack, else pop 
+         
+        # we can think of the algorithm as :
+        # If the parenthesis in my map, move forward if not just append
+        # if the stack is not empty, move forward if not return False
+        # if the map value of the parenthesis is equal to the top of the stack. POP
+        # if it is not equal to the top of the stack. APPEND
+        
+        
 class Solution(object):
     def isValid(self, s):
         myMap = {
@@ -6,16 +46,6 @@ class Solution(object):
             ']' : '['       
                  }
         stack = []
-        #Thought process
-        #If stack is empty and we encounter one in map, return false, if not in map, we append
-        #if its not empty, we append if its not in map or its in map but doesnt match top of stack, else pop
-        
-        
-        #we can think of the algorithm as.
-        # If the parenthesis in my map, move forward if not just append
-        # if the stack is not empty, move forward if not return False
-        # if the map value of the parenthesis is equal to the top of the stack. POP
-        # if it is not equal to the top of the stack. APPEND
         
         for i in range(len(s)):
             if s[i] in myMap:
---
+++ LeetCode 70 Climbing Stairs.py
@@ -0,0 +1,53 @@
+#You are climbing a staircase. It takes n steps to reach the top.
+
+# Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+# Example 1:
+
+# Input: n = 2
+# Output: 2
+# Explanation: There are two ways to climb to the top.
+# 1. 1 step + 1 step
+# 2. 2 steps
+# Example 2:
+
+# Input: n = 3
+# Output: 3
+# Explanation: There are three ways to climb to the top.
+# 1. 1 step + 1 step + 1 step
+# 2. 1 step + 2 steps
+# 3. 2 steps + 1 step
+ 
+
+# Constraints:
+
+# 1 <= n <= 45
+
+#Thought Process
+#If its one stair, theres only a way. just one step
+#If its two, there are two ways. two steps at a time or one twice
+#If its three, there are three ways. two then one, one then two, one three times
+#if its four, there are five. The idea is this follows a sequence. that the number of ways is the addition of the two previous number of ways
+#This is generally known as the Fibonnacci sequence and we can implement this with dynamic programming
+
+#Algorithm
+#for the first two, we know, we return n
+#to keep track of the rest, we create a dp array of size n+1, then set the dp[current index] to the previous one plus the one before that
+#then we return dp[n]
+
+class Solution(object):
+    def climbStairs(self, n): 
+        if n < 3: return n
+
+        dp = [0] * (n+1)
+        dp[1] = 1
+        dp[2] = 2
+        
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[n]
+    
+if __name__ == "__main__":
+    solution = Solution()
+    
+    print(solution.climbStairs(5))
