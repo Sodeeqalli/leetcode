3ce5f9d7768f5ba26649db83f84a9829e474600b
Alli Sodeeq <sodeeqalli@gmail.com>
2024-09-10 18:23:04 +0100

    question 151

--- Leetcode.py
+++
@@ -1,8 +0,0 @@
-nums = [1,2,3,4]
-newarray = [1] * len(nums)
-print(newarray)
-for i in range(len(nums)):
-    print(nums[i])
-    newarray = [ number for number in newarray if newarray.index(number) != i ]
-
-print(newarray)
